dec6934524c8a91a57eb6cefaa644f3b45c898f8
goge-goge <MayXD6@163.com>
2022-03-11 15:52:46 +0800

    change minting

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 // const ENDPOINT_ETHEREUM = `https://mainnet.infura.io/v3/${process.env.infuraKey}`;
 const ENDPOINT_RINKEBY = `https://rinkeby.infura.io/v3/${process.env.infuraKey}`;
 const ENDPOINT_POLYGON = 'https://polygon-rpc.com';
-const ENDPOINT_MUMBAI = 'https://rpc-mumbai.matic.today';
+const ENDPOINT_MUMBAI = `https://rinkeby.infura.io/v3/${process.env.infuraKey}`;
 export const RPC_URL =
   process.env.NODE_ENV === 'production' ? ENDPOINT_RINKEBY : ENDPOINT_MUMBAI;
